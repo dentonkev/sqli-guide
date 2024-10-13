d5e608ad787780d2a14ad3b5925a8d186555948c
Denton Kev <dentonkev122@gmail.com>
2024-10-13 13:02:29 +1100

    Change id to text

--- backend/src/db/questions.sql
+++ backend/src/db/questions.sql
@@ -1,8 +1,9 @@
--- q1
-INSERT INTO questions (query, answer, hints) 
+-- q1 question
+INSERT INTO questions (id, query, answer, hints) 
 VALUES (
-    'SELECT * FROM secrets WHERE session = "$input";',
-    'test" OR 1 = 1; -- ',
+    'q1', 
+    'SELECT flag FROM secrets WHERE question_id = ''$input'';',
+    'test'' OR 1 = 1; -- ',
     ARRAY [
         'An OR clause could be helpful here', 
         'After your input there still might be some code, try commenting it out', 
@@ -10,9 +11,16 @@ VALUES (
     ]
 );
 
+-- q1 asnwer
+INSERT INTO secrets (question_id, flag)
+VALUES ('q1', 
+    'SQLI_GUIDE{H1_TH3R3_7H1S_1S_FL3G_QU3S710N_0NE}'
+);
+
 -- q2
-INSERT INTO questions (query, answer, hints) 
+INSERT INTO questions (id, query, answer, hints) 
 VALUES (
+    'q2',
     '"SELECT username FROM users WHERE username = ''{username}'' and password = %s", (password,)',
     'test'' OR 1 = 1; -- ',
     ARRAY [
@@ -21,4 +29,10 @@ VALUES (
         'Comments in sql must be followed by a space',
         'Carefully look at string sytax in the provided SELECT statement'
     ]
+);
+
+-- q2 answer
+INSERT INTO secrets (question_id, flag)
+VALUES ('q2',
+    'SQLI_GUIDE{MUH1H11H1_F7A6_f04_Q2_L4L'
 );
--- backend/src/db/schema.sql
+++ backend/src/db/schema.sql
@@ -1,8 +1,13 @@
 DROP TABLE IF EXISTS questions;
-
 CREATE TABLE IF NOT EXISTS questions (
-    id SERIAL PRIMARY KEY,
-    query TEXT,
-    answer TEXT,
-    hints TEXT[]
+    id TEXT PRIMARY KEY,
+    query TEXT NOT NULL,
+    answer TEXT NOT NULL,
+    hints TEXT[] NOT NULL
+);
+
+DROP TABLE IF EXISTS secrets;
+CREATE TABLE IF NOT EXISTS secrets (
+    question_id TEXT PRIMARY KEY,
+    flag TEXT NOT NULL
 );
