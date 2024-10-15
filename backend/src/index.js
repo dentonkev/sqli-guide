734242809f2f002eac57b8cca5f986d34c68cc4e
Denton Kev <dentonkev122@gmail.com>
2024-10-16 02:26:00 +1100

    Remove test route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-import { config } from 'dotenv'; 
+import { config } from 'dotenv';
 config();
 import express from 'express';
 import cors from 'cors';
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // http://localhost:5000/
-const port = process.env.PORT ;
+const port = process.env.PORT;
 const host = process.env.HOST;
 
 app.use('/api/questions', router);
@@ -21,14 +21,3 @@ app.use('/api/questions', router);
 const server = app.listen(port, host, () => {
   console.log(`🌮Server started on port ${port} at ${host}🌮`);
 });
-
-
-// app.get('/test', async (req, res) => {
-//   try {
-//     const result = await pool.query('SELECT * FROM questions'); 
-//     res.json(result.rows[0]);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).json({ error: 'Database connection failed' });
-//   }
-// });
